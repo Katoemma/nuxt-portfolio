e30924bf96d942a4732599798fbc433f46f94361
kato emma <emmanuelkato39@gmail.com>
2024-07-18 15:27:37 +0300

    Added the sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  site: {
+    url: 'https://katoemma.netlify.app',
+  },
   app:{
     head:{
       meta:[
@@ -16,7 +19,5 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules:[
-    '@nuxtjs/tailwindcss'
-  ]
+  modules:['@nuxtjs/tailwindcss', "@nuxtjs/sitemap"]
 })
