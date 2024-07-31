cb18a4f53a02a7184cbc92b642c3490160a3afed
kato emma <emmanuelkato39@gmail.com>
2024-07-31 17:29:50 +0300

    Update the skilling padding

--- components/skillsSection.vue
+++ components/skillsSection.vue
@@ -3,11 +3,11 @@
 
 <template>
 <div class="bg-code-bg bg-cover">
-    <div class="flex flex-col items-center bg-black/70 text-white py-12">
+    <div class="flex flex-col items-center bg-black/70 text-white py-12 px-4">
         <UniversalScrollMouse />
         <UniversalHeading />
-        <p class="text-gray-300 mt-8 text-lg p-2">I am striving to never stop learning</p>
-        <div class="flex flex-col md:flex-row mt-8 gap-8 md:gap-24 mx-auto justify-between">
+        <p class="text-gray-300 mt-8 text-lg p-2 text-center">I am striving to never stop learning</p>
+        <div class="flex flex-col md:flex-row mt-8 gap-8 md:gap-24 mx-auto w-full md:w-auto justify-between">
             <SkillsSkill>
                 <template #icon>
                     <i class="fa fa-code text-2xl"></i>
